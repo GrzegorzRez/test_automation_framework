631d5f755aa6367abfde0585f3a3fe814872155d
Grzeche <rezmergrzegorz@gmail.com>
2023-03-06 18:52:28 +0100

    initial commit

---
+++ src/main/java/org/grzeche/App.java
@@ -0,0 +1,13 @@
+package org.grzeche;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
